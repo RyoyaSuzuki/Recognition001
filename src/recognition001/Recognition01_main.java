647c6f280c98d9e89dbf78860f2d1d4818a42f0d
1618054 <1618054@cs16054>
2018-12-13 16:54:46 +0900

    12/13

---
+++ src/recognition001/Recognition01_main.java
@@ -0,0 +1,49 @@
+package recognition001;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+
+import com.ibm.watson.developer_cloud.service.security.IamOptions;
+import com.ibm.watson.developer_cloud.visual_recognition.v3.VisualRecognition;
+import com.ibm.watson.developer_cloud.visual_recognition.v3.model.DetectFacesOptions;
+import com.ibm.watson.developer_cloud.visual_recognition.v3.model.DetectedFaces;
+
+public class Recognition01_main {
+	
+
+	public static void main(String[] args ) {
+		
+		VisualRecognition service = new VisualRecognition("2018-03-19");
+		IamOptions iamOptions = new IamOptions.Builder()
+		  .apiKey("aa")
+		  .build();
+		service.setIamCredentials(iamOptions);
+
+
+
+DetectFacesOptions detectFacesOptions = null;
+try {
+	detectFacesOptions = new DetectFacesOptions.Builder()
+						  .imagesFile(new File("img/aa.jpg"))
+						  .build();
+} catch (FileNotFoundException e) {
+	// TODO Auto-generated catch block
+	e.printStackTrace();
+}
+
+DetectedFaces result = service.detectFaces(detectFacesOptions).execute();
+				System.out.println(result);
+
+
+		
+		
+
+		
+
+
+
+
+
+	}
+
+}
