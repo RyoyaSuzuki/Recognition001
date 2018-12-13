be53fdb8e40cd088a50376041990d6570a7a0e5c
1618054 <1618054@cs16054>
2018-12-13 16:49:16 +0900

    12/13

---
+++ src/recognition001/Recognition001_main.java
@@ -0,0 +1,24 @@
+package recognition001;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+
+import com.ibm.watson.developer_cloud.service.security.IamOptions;
+import com.ibm.watson.developer_cloud.visual_recognition.v3.VisualRecognition;
+import com.ibm.watson.developer_cloud.visual_recognition.v3.model.DetectFacesOptions;
+import com.ibm.watson.developer_cloud.visual_recognition.v3.model.DetectedFaces;
+
+public class Recognition001_main {
+
+	public static void main(String[] args) {
+		
+		Recognition001_lib rlib = new Recognition001_lib();
+		
+		rlib.getResult("img/aa.jpg");
+		
+
+	
+	}
+
+		
+	}
